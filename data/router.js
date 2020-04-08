21f54b23bfa590387b97d03511f155170b08c649
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2020-04-08 17:00:59 -0400

    mvp meet after corrupt

--- data/router.js
+++ data/router.js
@@ -49,14 +49,93 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.put('/:id', (req, res) => {
-    const changes = req.body;
-    Data.update(req.params.id, changes)
-    if (!req.body.title || !req.body.contents) {
-        res
-          .status(400)
-          .json({ errorMessage: "Please provide title and contents for the post." })
+
+router.delete("/:id", (req, res) => {
+  let deletedPost;
+  Data.findById(req.params.id).then((post) => {
+    deletedPost = post;
+  });
+  Data.remove(req.params.id).then((item) => {
+    res.json(deletedPost);
+  });
+});
+
+
+
+router.put("/:id", (req, res) => {
+  let id = req.params.id;
+  Data.findById(id).then((post) => {
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    } else if (!req.body.title || !req.body.contents) {
+      res.status(400).json({ message: "Provide title and contents." });
+    } else {
+      Data.update(id, req.body)
+        .then((val) => {
+          Data.findById(id).then((updatedPost) => {
+            res.status(200).json(updatedPost);
+          });
+        })
+        .catch((err) =>
+          res
+            .status(500)
+            .json({ message: "The post information could not be modified." })
+        );
     }
-})
+  });
+});
+
+// /api/posts/:id/comments
+
+router.get("/:id/comments", (req, res) => {
+  Data.findById(req.params.id).then((post) => {
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    } else {
+      Data.findCommentById(req.params.id)
+        .then((comments) => {
+          res.status(200).json(comments);
+        })
+        .catch((err) => {
+          res.status(500).json({
+            message: "The comments information could not be retrieved.",
+          });
+        });
+    }
+  });
+});
+
+router.post("/:id/comments", (req, res) => {
+  Data.findById(req.params.id).then((post) => {
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    } else if (!req.body.text) {
+      res.status(400).json({ message: "Please provide text for the comment." });
+    } else {
+      Data.insertComment(req.body)
+        .then((comment) => {
+          Data.findCommentById(comment.id).then((comment) => {
+            res.status(201).json(comment);
+          });
+        })
+        .catch((err) => {
+          res
+            .status(500)
+            .json({
+              error:
+                "There was an error while saving the comment to the database.",
+            });
+          console.log(err);
+        });
+    }
+  });
+});
+
 
 module.exports = router;
